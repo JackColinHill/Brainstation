386cfe05ebbc9d4d1eef0b6443fb3855a890687f
JackColinHill <38734170+JackColinHill@users.noreply.github.com>
2018-04-29 13:05:54 -0400

    Add files via upload

---
+++ assignment-1.js
@@ -0,0 +1,15 @@
+//Assignmet 1: Basic JS
+//Jack Hill; April 29, 2018
+
+function assignment1(){
+    var numbers = [];
+    for (i=0;i<10;i++){
+        numbers.push(i);
+    }
+    console.log(numbers)
+    var last = numbers.pop()
+    console.log(last)
+    var car = {}
+    car.colour = "blank"
+}
+assignment1();
